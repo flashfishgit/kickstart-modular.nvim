b0ca9eebb339f0401ab2daab016205bd19f3c1eb
Marco Söllinger <102473074+flashfishgit@users.noreply.github.com>
2025-05-13 14:04:29 +0200

    m

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,20 +1,3 @@
 return {
   'github/copilot.vim',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  config = function()
-    require('copilot').setup {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = '<C-l>', -- Change to your preferred keybinding
-          next = '<C-n>',
-          prev = '<C-p>',
-          dismiss = '<C-]>',
-        },
-      },
-      panel = { enabled = false },
-    }
-  end,
 }
